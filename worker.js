f6b92468c265ac1a6e8a9275a4f138b8a3bdc9f8
mih777 <mih777777@yandex.ru>
2020-09-16 20:29:35 +0300

    server is almast ready except one thing

--- worker.js
+++ worker.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const express = require("express");
 const cors = require('cors')
 const pid = process.pid;
+let switch_cpu
   
 const app = express();
 app.use(cors())
@@ -20,7 +21,14 @@ app.post("/", jsonParser, function (req, res) {
      
     
     let result = req.body
-    console.log(result.length)
+    console.log(result)
+
+    if(result > 100){
+        switch_cpu = true
+    }else{
+        switch_cpu = false
+    }
+
     let log_str = `Отправлено обьектов: ${result.length}\n`
 
     fs.appendFileSync("log.txt", log_str);
@@ -40,3 +48,6 @@ const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Server started. Pid: ${pid} on port: ${port}`);
 });
+
+
+module.exports = switch_cpu
