72d5bfeece1261154ebf45a6c81828fe0a9b87d9
mih777 <mih777777@yandex.ru>
2020-09-16 20:50:19 +0300

    removed unnecessary

--- cluster.js
+++ cluster.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 3000
 
 if(cluster.isMaster) {
 
-    //const cpusCount = os.cpus.length
     console.log(`Master started Pid: ${pid} on port: ${port}`)
     for(let i = 0; i < os.cpus().length; i++){
         const worker = cluster.fork()
@@ -13,7 +12,6 @@ if(cluster.isMaster) {
             console.log(`worker died! Pid: ${worker.process.pid}`)
             cluster.fork()
         })
-        worker.send('Hello from server')
     }
     
 }
--- worker.js
+++ worker.js
@@ -2,48 +2,33 @@ const fs = require('fs')
 const express = require("express");
 const cors = require('cors')
 const pid = process.pid;
+const port = process.env.PORT || 3000
 let switch_cpu
   
 const app = express();
+
+// Access to fetch at 'http://localhost:3000/
 app.use(cors())
 // создаем парсер для данных в формате json
 const jsonParser = express.json();
-  
-app.get("/", function(req, res){
-      
-    var content = fs.readFileSync("objects.json", "utf8");
-    var objects = JSON.parse(content);
-    res.send(objects);
-});
-
-// получение отправленных данных
+// принимаем данные
 app.post("/", jsonParser, function (req, res) {
      
-    
     let result = req.body
     console.log(result)
 
-    if(result > 100){
-        switch_cpu = true
-    }else{
-        switch_cpu = false
-    }
-
+    if(result > 100){switch_cpu = true}else{switch_cpu = false}
+    console.log(switch_cpu)
     let log_str = `Отправлено обьектов: ${result.length}\n`
 
     fs.appendFileSync("log.txt", log_str);
 
     fs.writeFileSync("objects.json", JSON.stringify(result));
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-    res.json({
-        message: 'object created !', result
-    })
+    
+    res.json({ message: 'object created !', result })
 
 });
-
-
-const port = process.env.PORT || 3000 
+ 
 
 app.listen(port, () => {
     console.log(`Server started. Pid: ${pid} on port: ${port}`);
